cb60bc3afa67f6b3587ef2162e2464cb718e99c1
Alex <alex521@funnc.com>
2021-04-13 13:24:43 +0900

    프로젝트 생성

---
+++ AndroidMvvmSample/app/src/main/java/com/example/androidmvvmsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidmvvmsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
