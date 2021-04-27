0bf2ba75900f534df1e910976f8bacf41b43b7e4
tpghks521 <tpghks521@gmail.com>
2021-04-27 18:15:38 +0900

    데이터베이스 모듈 생성

---
+++ AndroidMvvmSample/app/src/main/java/com/example/androidmvvmsample/api/ApiService.kt
@@ -0,0 +1,4 @@
+package com.example.androidmvvmsample.api
+
+interface ApiService {
+}
--- AndroidMvvmSample/app/src/main/java/com/example/androidmvvmsample/di/AppModule.kt
+++ AndroidMvvmSample/app/src/main/java/com/example/androidmvvmsample/di/AppModule.kt
@@ -1,7 +1,28 @@
 package com.example.androidmvvmsample.di
 
+import android.content.Context
+import com.example.androidmvvmsample.api.ApiService
+import com.example.androidmvvmsample.data.AppDatabase
+import com.example.androidmvvmsample.data.repository.MainRepository
 import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 @Module
 class AppModule {
+
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return AppDatabase.getInstance(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMainRepository(): MainRepository {
+        return MainRepository()
+    }
+
 }
