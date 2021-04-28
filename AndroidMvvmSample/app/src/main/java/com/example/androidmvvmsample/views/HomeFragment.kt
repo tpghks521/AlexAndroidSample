bd153dd203bbf9f22a0778b38453fb92a8bbd51b
tpghks521 <tpghks521@gmail.com>
2021-04-28 23:01:51 +0900

    Home 클래스 생성

---
+++ AndroidMvvmSample/app/src/main/java/com/example/androidmvvmsample/views/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.androidmvvmsample.views
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment :Fragment() {
+}
